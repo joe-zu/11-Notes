5a7014392cc95c29fa76970048d8183742e82b9c
Joe Giampaoli <giampaoli.joe@gmail.com>
2021-03-01 12:18:24 -0800

    express working

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
